50b2af9fb97dcbe02cef4537c2e347470aed9fca
Mete Mertkan Dosemeci <m.dosemeci@tr.emakina.com>
2019-12-01 09:55:38 +0300

    Services tests implementation

---
+++ Clay.Tests/AttemptServiceTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Linq;
+using Clay.Constants;
+using Clay.Data;
+using Clay.Models.Domain;
+using Clay.Repositories.Interfaces;
+using Clay.Services.Implementation;
+using Moq;
+using Xunit;
+
+namespace Clay.Tests
+{
+    public class AttemptServiceTests
+    {
+        [Fact]
+        public void Get_User_Attempts_Should_Return_User_Attemps()
+        {
+            //Arrange
+            var pagedModel = new PagedModel();
+
+            var mockAttemptRepository = new Mock<IAttemptRepository>();
+
+            mockAttemptRepository.Setup(m => m.Attempts).Returns(new Attempt[] {
+                new Attempt {Id = 1,Action = Actions.LOCK,IsSuccessful = true,LockId = Guid.NewGuid(),UserId = "123",Time = DateTime.Now},
+                new Attempt {Id = 2,Action = Actions.LOCK,IsSuccessful = true,LockId = Guid.NewGuid(),UserId = "123",Time = DateTime.Now},
+                new Attempt {Id = 3,Action = Actions.UNLOCK,IsSuccessful = false,LockId = Guid.NewGuid(),UserId = "123",Time = DateTime.Now},
+                new Attempt {Id = 4,Action = Actions.UNLOCK,IsSuccessful = false,LockId = Guid.NewGuid(),UserId = "321",Time = DateTime.Now}
+            }.AsQueryable<Attempt>());
+
+            //Act
+            var target = new AttemptService(mockAttemptRepository.Object);
+            var pagedResult = target.GetUserAttempts("123",pagedModel);
+
+
+            //Assert
+            Assert.Equal(3, pagedResult.Results.Count);
+            Assert.Equal(1, pagedResult.Results.FirstOrDefault().Id);
+        }
+
+        [Fact]
+        public void Get_Lock_Attempts_Should_Return_Lock_Attemps()
+        {
+            //Arrange
+            var pagedModel = new PagedModel();
+            var lockId = Guid.NewGuid();
+            var mockAttemptRepository = new Mock<IAttemptRepository>();
+
+            mockAttemptRepository.Setup(m => m.Attempts).Returns(new Attempt[] {
+                new Attempt {Id = 1,Action = Actions.LOCK,IsSuccessful = true,LockId = lockId,UserId = "123",Time = DateTime.Now},
+                new Attempt {Id = 2,Action = Actions.LOCK,IsSuccessful = true,LockId = lockId,UserId = "123",Time = DateTime.Now},
+                new Attempt {Id = 3,Action = Actions.UNLOCK,IsSuccessful = false,LockId = Guid.NewGuid(),UserId = "123",Time = DateTime.Now},
+                new Attempt {Id = 4,Action = Actions.UNLOCK,IsSuccessful = false,LockId = Guid.NewGuid(),UserId = "321",Time = DateTime.Now}
+            }.AsQueryable<Attempt>());
+
+            //Act
+            var target = new AttemptService(mockAttemptRepository.Object);
+            var pagedResult = target.GetLockAttempts(lockId, pagedModel);
+
+
+            //Assert
+            Assert.Equal(2, pagedResult.Results.Count);
+            Assert.Equal(1, pagedResult.Results.FirstOrDefault().Id);
+        }
+
+        [Fact]
+        public void Get_All_Attemps_Returns_All_Attemps()
+        {
+            //Arrange
+            var pagedModel = new PagedModel();
+            var lockId = Guid.NewGuid();
+            var mockAttemptRepository = new Mock<IAttemptRepository>();
+
+            mockAttemptRepository.Setup(m => m.Attempts).Returns(new Attempt[] {
+                new Attempt {Id = 1,Action = Actions.LOCK,IsSuccessful = true,LockId = lockId,UserId = "123",Time = DateTime.Now},
+                new Attempt {Id = 2,Action = Actions.LOCK,IsSuccessful = true,LockId = lockId,UserId = "123",Time = DateTime.Now},
+                new Attempt {Id = 3,Action = Actions.UNLOCK,IsSuccessful = false,LockId = Guid.NewGuid(),UserId = "123",Time = DateTime.Now},
+                new Attempt {Id = 4,Action = Actions.UNLOCK,IsSuccessful = false,LockId = Guid.NewGuid(),UserId = "321",Time = DateTime.Now}
+            }.AsQueryable<Attempt>());
+
+            //Act
+            var target = new AttemptService(mockAttemptRepository.Object);
+            var pagedResult = target.GetAttempts(pagedModel);
+
+
+            //Assert
+            Assert.Equal(4, pagedResult.Results.Count);
+            Assert.Equal(1, pagedResult.Results.FirstOrDefault().Id);
+        }
+
+        [Fact]
+        public void Create_Attempt_Should_Create_Attempt()
+        {
+            //Arrange
+            var lockId = Guid.NewGuid();
+            var testAttempt= new Attempt
+            {
+                Id = 5,
+                Action = Actions.UNLOCK,
+                LockId = lockId,
+                UserId = "123",
+                IsSuccessful = true,
+                Time = DateTime.Now
+            };
+            var mockAttemptRepository = new Mock<IAttemptRepository>();
+
+            mockAttemptRepository.Setup(m => m.Attempts).Returns(new Attempt[] {
+                new Attempt {Id = 1,Action = Actions.LOCK,IsSuccessful = true,LockId = lockId,UserId = "123",Time = DateTime.Now},
+                new Attempt {Id = 2,Action = Actions.LOCK,IsSuccessful = true,LockId = lockId,UserId = "123",Time = DateTime.Now},
+                new Attempt {Id = 3,Action = Actions.UNLOCK,IsSuccessful = false,LockId = Guid.NewGuid(),UserId = "123",Time = DateTime.Now},
+                new Attempt {Id = 4,Action = Actions.UNLOCK,IsSuccessful = false,LockId = Guid.NewGuid(),UserId = "321",Time = DateTime.Now}
+            }.AsQueryable<Attempt>());
+
+            //Act
+            var target = new AttemptService(mockAttemptRepository.Object);
+            
+
+            try
+            {
+                target.CreateAttempt(testAttempt);
+                Assert.True(true);
+            }
+            catch
+            {
+                Assert.True(false);
+            }
+        }
+    }
+}
---
+++ Clay.Tests/LockServiceTests.cs
@@ -0,0 +1,144 @@
+﻿
+using System;
+using System.Linq;
+using Clay.Data;
+using Clay.Models.Domain;
+using Clay.Repositories.Interfaces;
+using Clay.Services.Implementation;
+using Moq;
+using Xunit;
+
+namespace Clay.Tests
+{
+    public class LockServiceTests
+    {
+        [Fact]
+        public void Get_All_Should_Return_All_Locks()
+        {
+            //Arrange
+            var pagedModel = new PagedModel();
+            var mockLockRepository = new Mock<ILockRepository>();
+            var mockUserLockRepository = new Mock<IUserLockRepository>();
+
+            mockLockRepository.Setup(m => m.Locks).Returns(new Lock[] {
+                new Lock{Id = Guid.NewGuid(),IsLocked = false,Name ="lock1",Place = "place1"},
+                new Lock{Id = Guid.NewGuid(),IsLocked = true,Name ="lock2",Place = "place2"},
+                new Lock{Id = Guid.NewGuid(),IsLocked = false,Name ="lock3",Place = "place3"},
+                new Lock{Id = Guid.NewGuid(),IsLocked = true,Name ="lock4",Place = "place4"}
+            }.AsQueryable<Lock>());
+
+            //Act
+            var target = new LockService(mockLockRepository.Object, mockUserLockRepository.Object);
+            var pagedResult = target.GetAll(pagedModel);
+
+            //Assert
+            Assert.Equal(4, pagedResult.Results.Count);
+            Assert.Equal("place1", pagedResult.Results.FirstOrDefault().Place);
+        }
+
+        [Fact]
+        public void Get_ById_Should_Return_One_Lock()
+        {
+            //Arrange
+            var testLock = new Lock
+            {
+                Id = Guid.NewGuid(),
+                Name = "test",
+                IsLocked = true,
+                Place = "test"
+            };
+            var mockLockRepository = new Mock<ILockRepository>();
+            var mockUserLockRepository = new Mock<IUserLockRepository>();
+
+            mockLockRepository.Setup(m => m.Locks).Returns(new Lock[] {
+                new Lock{Id = Guid.NewGuid(),IsLocked = false,Name ="lock1",Place = "place1"},
+                new Lock{Id = Guid.NewGuid(),IsLocked = true,Name ="lock2",Place = "place2"},
+                new Lock{Id = Guid.NewGuid(),IsLocked = false,Name ="lock3",Place = "place3"},
+                new Lock{Id = Guid.NewGuid(),IsLocked = true,Name ="lock4",Place = "place4"},
+                testLock
+            }.AsQueryable<Lock>());
+
+            //Act
+            var target = new LockService(mockLockRepository.Object, mockUserLockRepository.Object);
+            var result = target.GetById(testLock.Id);
+
+            //Assert
+            Assert.Equal(testLock.Id, result.Id);
+            Assert.Equal(testLock.Name, result.Name);
+        }
+
+        [Fact]
+        public void Get_ByName_Should_Return_One_Lock()
+        {
+            //Arrange
+            var testLock = new Lock
+            {
+                Id = Guid.NewGuid(),
+                Name = "test",
+                IsLocked = true,
+                Place = "test"
+            };
+            var mockLockRepository = new Mock<ILockRepository>();
+            var mockUserLockRepository = new Mock<IUserLockRepository>();
+
+            mockLockRepository.Setup(m => m.Locks).Returns(new Lock[] {
+                new Lock{Id = Guid.NewGuid(),IsLocked = false,Name ="lock1",Place = "place1"},
+                new Lock{Id = Guid.NewGuid(),IsLocked = true,Name ="lock2",Place = "place2"},
+                new Lock{Id = Guid.NewGuid(),IsLocked = false,Name ="lock3",Place = "place3"},
+                new Lock{Id = Guid.NewGuid(),IsLocked = true,Name ="lock4",Place = "place4"},
+                testLock
+            }.AsQueryable<Lock>());
+
+            //Act
+            var target = new LockService(mockLockRepository.Object, mockUserLockRepository.Object);
+            var result = target.GetByName(testLock.Name);
+
+            //Assert
+            Assert.Equal(testLock.Id, result.Id);
+            Assert.Equal(testLock.Name, result.Name);
+        }
+
+        [Fact]
+        public void Get_ByUserId_Should_Return_Users_Locks()
+        {
+            //Arrange
+            var testLock = new Lock
+            {
+                Id = Guid.NewGuid(),
+                Name = "test",
+                IsLocked = true,
+                Place = "test"
+            };
+            var pagedModel= new PagedModel();
+            var exampleUserId = Guid.NewGuid();
+            var mockLockRepository = new Mock<ILockRepository>();
+            var mockUserLockRepository = new Mock<IUserLockRepository>();
+
+            mockLockRepository.Setup(m => m.Locks).Returns(new Lock[] {
+                new Lock{Id = Guid.NewGuid(),IsLocked = false,Name ="lock1",Place = "place1"},
+                new Lock{Id = Guid.NewGuid(),IsLocked = true,Name ="lock2",Place = "place2"},
+                new Lock{Id = Guid.NewGuid(),IsLocked = false,Name ="lock3",Place = "place3"},
+                new Lock{Id = Guid.NewGuid(),IsLocked = true,Name ="lock4",Place = "place4"},
+                testLock
+            }.AsQueryable<Lock>());
+
+            mockUserLockRepository.Setup(m => m.UserLocks).Returns(new UserLock[]
+            {
+                new UserLock
+                {
+                    LockId = testLock.Id,
+                    UserId = exampleUserId.ToString()
+                }, 
+            }.AsQueryable<UserLock>());
+
+
+            //Act
+            var target = new LockService(mockLockRepository.Object, mockUserLockRepository.Object);
+            var result = target.GetByUserId(exampleUserId.ToString(),pagedModel);
+
+            //Assert
+            Assert.Equal(1,result.Count);
+            Assert.Equal("test",result.FirstOrDefault().Name);
+        }
+    }
+}
