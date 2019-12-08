50d28d8aeadd86a414959a0f3e2234de783a8ec6
Mete Mertkan Dosemeci <m.dosemeci@tr.emakina.com>
2019-12-08 22:16:11 +0300

    Cleanup

--- Clay/Constants/CacheKeys.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Clay.Constants
-{
-    public static class CacheKeys
-    {
-        public const string DELIMITER = "_";
-        public const string GETLOCKS = "GetLocks";
-        public const string GETATTEMPTS = "GetAttempts";
-    }
-}
